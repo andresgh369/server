5dac2e1f2d4aa45f201660c618397bef412efc49
andresgh369 <44000958+andresgh369@users.noreply.github.com>
2018-11-28 01:31:12 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@ze4x1em_8!ol!4nx&xfp%!a=@#u$rc$m&+=6fo-akj3^3jh-*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://andresgh369.pythonanywhere.com']
+ALLOWED_HOSTS = ['andresgh369.pythonanywhere.com/perro/']
 
 
 # Application definition
